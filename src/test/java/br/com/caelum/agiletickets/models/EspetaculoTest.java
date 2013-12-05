7ce04ded951123d02fc4f54a9298d2035ec0567d
GECOS <agil4037@caelum-20120929-1.(none)>
2013-12-05 09:10:07 -0200

    commit

--- src/test/java/br/com/caelum/agiletickets/models/EspetaculoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/EspetaculoTest.java
@@ -3,7 +3,15 @@ package br.com.caelum.agiletickets.models;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
+import java.util.List;
+
+import org.joda.time.DateTime;
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
+import org.junit.Assert;
 import org.junit.Test;
+
+import sun.util.resources.LocaleData;
 
 public class EspetaculoTest {
 
@@ -79,5 +87,60 @@ public class EspetaculoTest {
 		sessao.setIngressosReservados(quantidade);
 
 		return sessao;
+	}
+	
+	@Test
+	public void deveCriarApenasUmaSessaoParaMesmoDia() {
+		LocalDate dataAtual = new LocalDate();
+		LocalTime horario = new LocalTime(21,00);
+		DateTime dataHora = dataAtual.toDateTime(horario);
+		Espetaculo espetaculo = new Espetaculo();
+		List<Sessao> sessoes = espetaculo.criaSessoes(dataAtual, dataAtual, horario, Periodicidade.DIARIA);
+		Assert.assertEquals(1, sessoes.size());
+		Assert.assertEquals(dataHora, sessoes.get(0).getInicio());		 
+	}
+	
+	@Test
+	public void naoDeveCriarSessaoQuandoInicioMaiorQueFinal() {
+		LocalDate dataInicio = new LocalDate().plusDays(1);
+		LocalDate dataFinal = new LocalDate();
+		LocalTime horario = new LocalTime(21,00);
+		Espetaculo espetaculo = new Espetaculo();
+		List<Sessao> sessoes = espetaculo.criaSessoes(dataInicio, dataFinal, horario, Periodicidade.DIARIA);
+		Assert.assertEquals(0, sessoes.size());
+	}
+	
+	@Test
+	public void deveCriarCincoSessoesDiarias() {
+		LocalDate dataInicio = new LocalDate();
+		LocalDate dataFinal = new LocalDate().plusDays(4);		
+		LocalTime horario = new LocalTime(21,00);
+		Espetaculo espetaculo = new Espetaculo();
+		List<Sessao> sessoes = espetaculo.criaSessoes(dataInicio, dataFinal, horario, Periodicidade.DIARIA);
+		Assert.assertEquals(5, sessoes.size());
+		//int i=0;
+		//for (Sessao sessao : sessoes) {
+		Sessao sessao = new Sessao();
+		for(int i=0;i<sessoes.size();i++) {			
+		    sessao=sessoes.get(i);
+			DateTime dataHora = dataInicio.plusDays(i).toDateTime(horario);
+			Assert.assertEquals(dataHora, sessao.getInicio());
+			//i++;
+		}		
+	}
+	
+	public void deveCriarCincoSessoesSemanais() {
+		LocalDate dataInicio = new LocalDate();
+		LocalDate dataFinal = new LocalDate().plusWeeks(5);		
+		LocalTime horario = new LocalTime(21,00);
+		Espetaculo espetaculo = new Espetaculo();
+		List<Sessao> sessoes = espetaculo.criaSessoes(dataInicio, dataFinal, horario, Periodicidade.SEMANAL);
+		Assert.assertEquals(5, sessoes.size());
+		int i=0;
+		for (Sessao sessao : sessoes) {
+			DateTime dataHora = dataInicio.plusWeeks(i).toDateTime(horario);
+			Assert.assertEquals(dataHora, sessao.getInicio());
+			i++;
+		}	
 	}
 }
